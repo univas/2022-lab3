b3b0b99fae5497532d0313bd763ee584027943cd
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-03-22 21:57:22 -0300

    Test

--- exercicio-02/src/br/edu/univas/vo/FuncionarioController.java
+++ exercicio-02/src/br/edu/univas/vo/FuncionarioController.java
@@ -1,8 +1,26 @@
 package br.edu.univas.vo;
 
+import java.util.Scanner;
+
 public class FuncionarioController {
 
     public Funcionario createFuncionario() {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+
+        Funcionario f1 = new Funcionario();
+        System.out.println("Digite o registro");
+        f1.setRegistro(scanner.nextLine());
+
+        System.out.println("Digite o nome:");
+        f1.setNome(scanner.nextLine());
+
+        System.out.println("Digite o email:");
+        f1.setEmail(scanner.nextLine());
+
+        DepartamentoController dController = new DepartamentoController();
+        Departamento d1 = dController.createDepartamento();
+
+        f1.setDepartamento(d1);
+        return f1;
     }
 }
