935fa4fd2bab5889af27e92b6d959441660f0fda
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-04-28 20:09:39 -0300

    Add ENUM

--- exercicio-04/src/br/edu/univas/vo/Professor.java
+++ exercicio-04/src/br/edu/univas/vo/Professor.java
@@ -3,9 +3,26 @@ package br.edu.univas.vo;
 public class Professor implements Pesquisador {
 
     private String nome;
+    private Formacao formacao;
 
     @Override
     public int calcularConceito() {
         return 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Formacao getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(Formacao formacao) {
+        this.formacao = formacao;
     }
 }
